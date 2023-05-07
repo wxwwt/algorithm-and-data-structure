4c57fb19907cbf5f13c0a39d4b2474c83e88f4a8
shenchuang <908181134@qq.com>
2023-05-07 20:35:47 +0800

    feat:剑指offer的几道题目

---
+++ src/main/java/com/algorithm/leetcode/leetcode/DeleteNode_18.java
@@ -0,0 +1,29 @@
+package com.algorithm.leetcode.leetcode;
+
+/**
+ * @author scott
+ * @date 2023/5/7 20:21
+ */
+public class DeleteNode_18 {
+
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode vNode = new ListNode(Integer.MIN_VALUE);
+        vNode.next = head;
+        ListNode curNode = vNode;
+        while(curNode.next != null) {
+            if (curNode.next.val == val) {
+                ListNode next = curNode.next.next;
+                curNode.next = next;
+                break;
+            }
+           curNode = curNode.next ;
+        }
+        return curNode.next;
+    }
+}
+
+ class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
