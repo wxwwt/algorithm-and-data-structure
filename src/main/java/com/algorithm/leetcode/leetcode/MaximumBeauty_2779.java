ad0137987a9590dac6ef9da730b7e796a5f6dd4a
shenchuang <908181134@qq.com>
2023-07-22 21:04:33 +0800

    DP 2770 空间复杂度O(n) -> O(1)

--- src/main/java/com/algorithm/leetcode/leetcode/MaximumBeauty_2779.java
+++ src/main/java/com/algorithm/leetcode/leetcode/MaximumBeauty_2779.java
@@ -11,19 +11,34 @@ import java.util.Arrays;
  */
 public class MaximumBeauty_2779 {
 
-    public int maximumBeauty(int[] nums, int k) {
-        int left = 0, right = 0;
-        Arrays.sort(nums);
-        int max = 0;
-        for (int i = 0; i < nums.length;i++) {
-            while (right <= nums.length - 1 && nums[right] - nums[left] <= 2 * k) {
-                right++;
-            }
+//    public int maximumBeauty(int[] nums, int k) {
+//        int left = 0, right = 0;
+//        Arrays.sort(nums);
+//        int max = 0;
+//        for (int i = 0; i < nums.length;i++) {
+//            while (right <= nums.length - 1 && nums[right] - nums[left] <= 2 * k) {
+//                right++;
+//            }
+//
+//                left++;
+//            max = Math.max(max, right - left + 1);
+//        }
+//        return max;
+//    }
 
-                left++;
-            max = Math.max(max, right - left + 1);
+
+    public int maximumBeauty(int[] nums, int k) {
+        Arrays.sort(nums);
+        int s = 0, count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[s] + k < nums[i] - k ) {
+                // 不满足的话左指针右移
+                s++;
+            }
+            count = Math.max(i - s + 1, count);
         }
-        return max;
+        return count;
     }
 
 
