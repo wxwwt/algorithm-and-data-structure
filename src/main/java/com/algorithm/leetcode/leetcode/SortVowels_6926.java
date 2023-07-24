fbd1f49f5ab79781c4e903fc18477b1f0dab3910
shenchuang <908181134@qq.com>
2023-07-24 23:59:30 +0800

    2789  贪心+反向遍历

---
+++ src/main/java/com/algorithm/leetcode/leetcode/SortVowels_6926.java
@@ -0,0 +1,49 @@
+package com.algorithm.leetcode.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @author scott
+ * @date 2023/7/22 22:27
+ */
+public class SortVowels_6926 {
+
+    static Set<Character> set = Set.of('A','E','I', 'O','U','a','e','i','o','u');
+
+    public String sortVowels(String s) {
+
+                char[] arr = s.toCharArray();
+                List<Integer> list = new ArrayList<>();
+
+            for (int i =0; i < s.length(); i++) {
+                if (set.contains(arr[i])) {
+                    list.add(i);
+
+                }
+            }
+
+            if (list.isEmpty()) {
+                return s;
+            }
+
+        char[] cArr = new char[list.size()];
+            for (int i = 0; i < list.size();i++) {
+                cArr[i] = arr[list.get(i)];
+            }
+           Arrays.sort(cArr);
+            for (int i = 0;  i < list.size() ; i++) {
+                arr[list.get(i)] = cArr[i];
+
+            }
+            return new String(arr);
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertEquals("lYmpH", sortVowels( "lYmpH"));
+        Assert.assertEquals("lEOtcede", sortVowels( "lEetcOde"));
+    }
+}
